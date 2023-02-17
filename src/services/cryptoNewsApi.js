cae6630694f241c91d1b63b46fcbf85fa8bf1a4e
finding-disha <dishaukhlikar47@gmail.com>
2023-02-17 19:48:55 +0530

    created crypto app

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '3f926cb9acmshd5be8944fadef69p178763jsn877966c307df',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://bing-news-search1.p.rapidapi.com' }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&count=${count}`),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
